aec1840094686ba3a8f8ad75f66fa081ecb90632
madhuravani7 <madhubudige67@gmail.com>
2024-07-18 09:27:22 +1000

    UI finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from './Components/Header'
 import Balance from './Components/Balance'
 import IncomeExpenses from './Components/IncomeExpenses'
+import TransactionList from './Components/TransactionList'
+import AddTransaction from './Components/AddTransaction'
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <div className="container">
         <Balance/>
         <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
       </div>
     </div>
   )
